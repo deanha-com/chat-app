3aa8bce4956254cf905d409bed377517afb332f5
Dean H <dwha88@gmail.com>
2021-04-28 00:01:02 +0100

    added time stamps

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+
 if (messageForm != null) {
   const name = prompt('What is your name?')
   appendMessage('You joined as ' + name, socket.id, 'joined')
@@ -14,7 +15,7 @@ if (messageForm != null) {
     const message = messageInput.value
 
     if ( message != '' ) { // send message if input is not empty
-      appendMessage(`${message}`, socket.id)
+      appendMessage(`${message} <span class='ts'>${getCurrentTime()}</span>`, socket.id)
       socket.emit('send-chat-message', roomName, message)
       messageInput.value = ''
       document.getElementById("message-input").focus();
@@ -32,8 +33,10 @@ socket.on('room-created', room => {
 	roomContainer.append(roomLink)
 })
 
+
+
 socket.on('chat-message', data => {
-  appendMessage(`<span>${data.name} </span>: ${data.message}`)
+  appendMessage(`<span>${data.name}</span><span class='user-socketid'>(${socket.id})</span>: ${data.message} <span class='ts'>${getCurrentTime()}</span>`)
   window.scrollTo(0,document.body.scrollHeight)
   document.getElementById("message-input").focus();
 })
@@ -63,3 +66,23 @@ function appendMessage(message, userid, cssClass) {
   messageContainer.append(messageElement)
   window.scrollTo(0,document.body.scrollHeight); // Scroll down 
 }
+
+function addZero(i) {
+	if (i < 10) {
+	  i = "0" + i;
+	}
+	return i;
+  }
+
+function getCurrentTime() {
+	var d = new Date();
+	var h = addZero(d.getHours());
+	var m = addZero(d.getMinutes());
+	var suffix = '';
+	if (h >= 12) {
+	 suffix = 'pm'
+	} else {
+	suffix = 'am'
+	}
+	return h + ":" + m + suffix;
+  }
